a07443aebb8383966bf5724808845420923edd0d
Ramani, Vivek (NIH/NCI) [C] <ramaniv@mail.nih.gov>
2016-06-16 17:42:37 -0400

    specimen_shipped message tests

--- uMATCH/PedMATCH/features/support/patient_helper_methods.rb
+++ uMATCH/PedMATCH/features/support/patient_helper_methods.rb
@@ -31,7 +31,7 @@ class Patient_helper_methods
 
   def Patient_helper_methods.createSpecimenRequest(params={})
     msgHash = JSON.parse(params['msg'])
-    specimenHash = msgHash['specimens_received']
+    specimenHash = msgHash['specimen_received']
     if !params['receivedDate'].nil?
       @reportedDate = Helper_Methods.getDateAsRequired(params['receivedDate'])
       specimenHash['received_date'] = @reportedDate
@@ -92,6 +92,74 @@ class Patient_helper_methods
     end
     return msgHash.to_json
   end
+
+  def Patient_helper_methods.createSpecimenShippedMessageRequest(params={})
+    msgHash = JSON.parse(params['msg'])
+    specimenHash = msgHash['specimen_shipped']
+    p specimenHash
+    if !params['shipped_date'].nil?
+      @shippedDate = Helper_Methods.getDateAsRequired(params['shipped_date'])
+      specimenHash['shipped_date'] = @shippedDate
+    end
+
+    return msgHash.to_json
+  end
+
+  def Patient_helper_methods.create_new_specimen_shipped_message(psn,type,surgical_id,molecular_id,datePreference)
+    dateCreated = Helper_Methods.getDateAsRequired(datePreference)
+
+    header_hash = {"msg_guid"=>"0f8fad5b-d9cb-469f-al65-80067728950e",
+                   "msg_dttm"=>dateCreated
+    }
+
+    internal_use_Hash = {"stars_patient_id"=>psn,
+                         "stars_specimen_id_dna"=>surgical_id
+    }
+
+    if type == 'BLOOD_DNA'
+      specimen_hash={"study_id"=>'APEC1621',
+                     "patient_id"=> psn,
+                     "type"=>type,
+                     "surgical_event_id"=>surgical_id,
+                     "molecular_id"=>molecular_id,
+                     "molecular_dna_id"=>molecular_id+'D',
+                     "shipped_date"=>dateCreated,
+                     "carrier"=> "Federal Express",
+                     "tracking_id"=> "7956 4568 1235",
+                     "internal_use_only"=>internal_use_Hash
+      }
+    elsif type == 'TISSUE_DNA_AND_CDNA'
+      specimen_hash={"study_id"=>'APEC1621',
+                     "patient_id"=> psn,
+                     "type"=>type,
+                     "surgical_event_id"=>surgical_id,
+                     "molecular_id"=>molecular_id,
+                     "molecular_dna_id"=>molecular_id+'D',
+                     "molecular_cdna_id"=>molecular_id+'C',
+                     "shipped_date"=>dateCreated,
+                     "carrier"=> "Federal Express",
+                     "tracking_id"=> "7956 4568 1235",
+                     "internal_use_only"=>internal_use_Hash
+      }
+    elsif type == 'SLIDE'
+      specimen_hash={"study_id"=>'APEC1621',
+                     "patient_id"=> psn,
+                     "type"=>type,
+                     "surgical_event_id"=>surgical_id,
+                     "shipped_date"=>dateCreated,
+                     "carrier"=> "Federal Express",
+                     "tracking_id"=> "7956 4568 1235",
+                     "internal_use_only"=>internal_use_Hash
+      }
+    end
+
+    specimen_shipped_hash ={"header"=>header_hash,
+                            "specimen_received"=>specimen_hash
+    }
+    return specimen_shipped_hash.to_json
+  end
+
+
 end
 
 
