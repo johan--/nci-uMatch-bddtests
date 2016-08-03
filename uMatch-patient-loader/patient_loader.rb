1bddb802a8a89ca17ebd54f74ee6ab73a25a378e
LiWang <adamastwl@gmail.com>
2016-08-03 15:35:24 -0400

    1. Changed folder name from DeleteScript to DataSetup, and changed relative travis code 2. Optimized patient_loader

--- uMatch-patient-loader/patient_loader.rb
+++ uMatch-patient-loader/patient_loader.rb
@@ -5,19 +5,19 @@ class PatientLoader
   include HTTParty
 
   # cmd line: ruby -r "./patient_loader.rb" -e "PatientLoader.load_patient '3344'"
-  def self.load_patient_to_local(patient_id, waitTime)
+  def self.load_patient_to_local(patient_id, wait_time)
     p "Loading patient #{patient_id}"
-    raise "patient_id must be valid" if patient_id.nil? || patient_id.length == 0
+    raise 'patient_id must be valid' if patient_id.nil? || patient_id.length == 0
     file = File.read("patients/#{patient_id}.json")
 
     message_list = JSON.parse(file)
     p "There are #{message_list.length} json messages in patient json file. Processing..."
 
-    allItems = 0
+    all_items = 0
     failure = 0
-    failList = 'Failed message IDs: '
+    fail_list = 'Failed message IDs: '
     message_list.each do |message|
-      allItems += 1
+      all_items += 1
       p ''
       # p "Loading message #{message.to_json}..."
       if message.key?('sleep')
@@ -32,19 +32,19 @@ class PatientLoader
         output = `#{curl_cmd}`
         p ''
         p "Output from running curl: #{output}"
-        if !output.downcase.include?'success'
+        unless output.downcase.include?'success'
           p 'Failed'
           puts JSON.pretty_generate(message)
           failure += 1
-          failList = "#{failList} No.#{allItems}"
+          fail_list = "#{fail_list} No.#{all_items}"
         end
-        sleep(waitTime)
+        sleep(wait_time)
       end
     end
 
-    pass = allItems - failure
+    pass = all_items - failure
     p ''
-    p "#{allItems} messages processed, #{pass} passed and #{failure} failed"
+    p "#{all_items} messages processed, #{pass} passed and #{failure} failed"
   end
 
   def self.configure
@@ -58,31 +58,12 @@ class PatientLoader
 
 
   def self.backup_local_db(table_name)
-    cmd = "aws dynamodb scan --table-name #{table_name} --endpoint-url http://localhost:8000 > nci_match_bddtests_#{table_name}_seed_data.json"
+    cmd = "aws dynamodb scan --table-name #{table_name} --endpoint-url http://localhost:8000 > ../DataSetup/patient_seed_data/nci_match_bddtests_#{table_name}_seed_data.json"
     `#{cmd}`
   end
 
-  def self.local_patient_to_aws
-    NciMatchPatientModels::Patient.set_table_name 'patient'
-    local_patient_json = JSON.parse(File.read('nci_match_bddtests_patient_seed_data.json'))
-    items = local_patient_json['Items']
-    p items.is_a?(Array)
-    items.each do |this_patient|
-      patient = NciMatchPatientModels::Patient.new
-      this_patient.keys.each do |key|
-        if patient.respond_to?(key)
-          patient.send("#{key}=", this_patient[key].values[0])
-        else
-          p "Patient class doesn't contains attribute #{key}"
-        end
-      end
-      patient.save
-
-    end
-  end
-
   def self.local_backup_to_aws(table_name)
-    class_name = "NciMatchPatientModels::#{table_name.camelcase}"
+    class_name = "NciMatchPatientModels::#{table_name.camelize}"
     clazz = class_name.constantize
     clazz.send('set_table_name', table_name.downcase)
     local_json = JSON.parse(File.read("nci_match_bddtests_#{table_name}_seed_data.json"))
@@ -100,7 +81,8 @@ class PatientLoader
           next
         end
 
-        if this_item[key].keys[0]!='M'&&this_item[key].keys[0]!='S'
+        acceptable_keys = %w(M S BOOL N) 
+        unless acceptable_keys.include?(this_item[key].keys[0])
           p "#{class_name}-#{key} has invalid key #{this_item[key].keys[0]}"
           return
         end
@@ -118,29 +100,18 @@ class PatientLoader
         p "#{class_name} doesn't have save method!"
         return
       end
-
     end
+    p "#{table_name}: Local to aws done!"
   end
 
   def self.backup_all_local_patient_db
-    backup_local_db('patient')
-    backup_local_db('specimen')
-    backup_local_db('event')
-    backup_local_db('shipment')
-    backup_local_db('variant')
-    backup_local_db('variant_report')
+    %w(patient specimen event shipment variant variant_report).each { |table_name| backup_local_db(table_name) }
     p 'Done!'
   end
 
   def self.upload_local_backups_to_aws
     configure
-    # local_backup_to_aws('patient')
-    # local_backup_to_aws('specimen')
-    # local_backup_to_aws('shipment')
-    #
-    # local_backup_to_aws('event')
-    local_backup_to_aws('variant')
-    # local_backup_to_aws('variant_report')
-    p 'Done!'
+    %w(patient specimen event shipment variant variant_report).each { |table_name| local_backup_to_aws(table_name) }
+    p 'All local to aws works done!'
   end
 end
