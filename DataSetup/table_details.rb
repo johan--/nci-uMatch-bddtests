e79e417bcd4bc50416a8def0dc76dbd2a62f55ae
Raseel Mohamed <raseel.mohamed@nih.gov>
2016-08-04 13:41:24 -0400

    Adding missed file

---
+++ DataSetup/table_details.rb
@@ -0,0 +1,42 @@
+class TableDetails
+
+  TREATMENT_ARM = {
+      name: 'treatment_arm',
+      keys: %w(name date_created)
+  }
+
+  PATIENT = {
+      name: 'patient',
+      keys: %w(patient_id registration_date)
+  }
+
+  ASSIGNMENT = {
+      name: 'assignment',
+      keys: %w(patient_id assigned_date)
+  }
+
+  EVENT = {
+      name: 'event',
+      keys: %w(entity_id event_date)
+  }
+
+  SHIPMENT = {
+      name: 'shipment',
+      keys: %w(uuid shipped_date)
+  }
+
+  SPECIMEN = {
+      name: 'specimen',
+      keys: %w(patient_id collected_date)
+  }
+
+  VARIANT = {
+      name: 'variant',
+      keys: %w(uuid)
+  }
+
+  VARIANT_REPORT = {
+      name: 'variant_report',
+      keys: %w(surgical_event_id variant_report_received_date)
+  }
+end
