bd9107f8231083471dbb58b6eb465fbe7b6a038d
Raseel Mohamed <raseel.mohamed@nih.gov>
2016-09-01 23:27:42 -0400

    adding logging for test

--- nci-match-ui-bddtests/pages/loginPage.js
+++ nci-match-ui-bddtests/pages/loginPage.js
@@ -11,8 +11,12 @@ var LoginPage = function() {
     var accessbtn =  element(by.css('button[type="submit"]'));
 
     this.goToLoginPage = function(){
-        browser.get('/#/auth/login', 6000);
+        browser.get('/#/auth/login', 6000).then(function () {
+            browser.waitForAngular();
+        });
         browser.getCurrentUrl().then(function(url){
+            console.log("current_url = " + url);
+            console.log("url !== '/#/auth/login' " + (url.match('/#/auth/login')));
             if(url !== '/#/auth/login'){
                 dashboard.logout;
             }
@@ -28,9 +32,12 @@ var LoginPage = function() {
         accessbtn.click(); // Clicking NCI-Matchbox button
         browser.sleep(1000);
         browser.isElementPresent(previousLoginLink).then(function (present){
+            console.log("value of present = "+present);
             if (present === true) {
+                console.log("clicking");
                 previousLogin.click();
             } else {
+                console.log("entering values");
                 email.sendKeys(username);
                 pass.sendKeys(password);
                 loginbtn.click();
