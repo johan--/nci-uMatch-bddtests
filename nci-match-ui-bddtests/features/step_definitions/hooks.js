a1d6d15284d428219d1ad4ce37c7828311b8f72d
Ramani, Vivek (NIH/NCI) [C] <ramaniv@mail.nih.gov>
2016-09-22 19:20:29 -0400

    trying to capture screenshot

--- nci-match-ui-bddtests/features/step_definitions/hooks.js
+++ nci-match-ui-bddtests/features/step_definitions/hooks.js
@@ -6,7 +6,7 @@ module.exports = function() {
     this.After(function (scenario, callback) {
         if (scenario.isFailed()) {
             browser.takeScreenshot().then(function (png) {
-                var img = new Buffer(png, 'base64').toString('binary');
+                var img = new Buffer(png, 'base64');
                 scenario.attach(img, 'image/png', callback);
             }, function(err){
                 callback(err);
@@ -17,16 +17,16 @@ module.exports = function() {
         }
     });
 
-     // this.AfterFeatures(function () {
-     //     var Reporter = require('cucumber-html-report');
-     //     var options = {
-     //         source: process.cwd() + '/../results/result.json',
-     //         dest:   '/Users/mohamedr2/Desktop/RESULTS_FOLDER',
-     //         name:   'report.html',
-     //         title:  'Results in html'
-     //     };
-
-     //     var report = new Reporter(options);
-     //     report.createReport();
-     // })
+      //this.AfterFeatures(function () {
+      //    var Reporter = require('cucumber-html-report');
+      //    var options = {
+      //        source: process.cwd() + '/../results/result.json',
+      //        dest:   '/../results',
+      //        name:   'report.html',
+      //        title:  'Results in html'
+      //    };
+      //
+      //    var report = new Reporter(options);
+      //    report.createReport();
+      //})
 };
