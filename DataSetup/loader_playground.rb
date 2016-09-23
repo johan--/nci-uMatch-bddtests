2a287f00df9079402531a399fa1ba93c5950321c
LiWang <adamastwl@gmail.com>
2016-09-23 11:19:16 -0400

    Added load_playground file There are several useful scripts in this file, you can use the script clear local dynamodb and load seed data into it or clear aws dynamodb and load seed data into it

---
+++ DataSetup/loader_playground.rb
@@ -0,0 +1,19 @@
+require_relative 'patient_message_loader'
+require_relative 'treatment_arm_message_loader'
+require_relative 'dynamo_data_upload'
+require_relative 'dynamo_delete_script'
+
+############clear local dynamodb and load all seed data
+# DynamoDb.new('local').clear_all_tables
+# DynamoDataUploader.new('local').upload_treatment_arm_to_aws
+# DynamoDataUploader.new('local').upload_patient_data_to_aws
+
+############clear local treatment arm related tables
+# DynamoDb.new('local').clear_table('treatment_arm')
+# DynamoDb.new('local').clear_table('treatment_arm_patient')
+
+
+############clear aws dynamodb and load all seed data
+# DynamoDb.new('default').clear_all_tables
+# DynamoDataUploader.new('default').upload_patient_data_to_aws
+# DynamoDataUploader.new('default').upload_treatment_arm_to_aws
