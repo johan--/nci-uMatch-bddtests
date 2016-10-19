24920e48419dfe6e9a7b0576256e632e9d77fca7
Raseel Mohamed <raseel.mohamed@nih.gov>
2016-10-19 14:43:16 -0400

    adding more corrections to TA seed json, also adding env variable to config file.

--- nci-match-ui-bddtests/config.js
+++ nci-match-ui-bddtests/config.js
@@ -4,7 +4,7 @@
 var helper = require('./support/setup');
 
 exports.config = {
-    baseUrl: 'https://pedmatch-int.nci.nih.gov',  //when developing tests use http://localhost:9000',
+    baseUrl: process.env.UI_HOSTNAME,  //when developing tests use http://localhost:9000',
 
     //seleniumAddress: 'http://localhost:4444/wd/hub',
     chromeOnly: true,
