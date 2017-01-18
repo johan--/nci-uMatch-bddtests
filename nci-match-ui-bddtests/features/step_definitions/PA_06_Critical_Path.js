d78195ef95fa711621e424bbcd6829c847d4268a
Rick Zakharov <rick.zakharov@gmail.com>
2017-01-18 16:28:19 -0500

    Working on role based BDD tests

--- nci-match-ui-bddtests/features/step_definitions/PA_06_Critical_Path.js
+++ nci-match-ui-bddtests/features/step_definitions/PA_06_Critical_Path.js
@@ -529,43 +529,4 @@ module.exports = function () {
             }).then(callback);
         }
     });
-
-    this.When(/^The variant comment buttons are displayed$/, function (callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
-    });
-    
-    this.Then(/^I can click on the variant comment button$/, function (callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
-    });
-
-    this.Then(/^I should see the variant comment dialog$/, function (callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
-    });
-
-    this.Then(/^I "(should( not)?)" be able to edit the comment$/, function (shouldOrNot, callback) {
-        var should = shouldOrNot === 'should';
-
-        var variantElem = patientPage.variant('', '');
-        if (should) {
-        } else {
-        }
-  
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
-    });
-
-    this.Then(/^I "(should( not)?)" be able to click OK button$/, function (shouldOrNot, callback) {
-        var should = shouldOrNot === 'should';
-
-        var button = patientPage.button('OK');
-        if (should) {
-        } else {
-        }
-
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
-    });
 };
