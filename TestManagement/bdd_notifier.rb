0350ce5ad53e43d9ee5fb84a407c559f205aa32f
LiWang <adamastwl@gmail.com>
2017-02-10 16:39:14 -0500

    Updated BDD Notifier

--- TestManagement/bdd_notifier.rb
+++ TestManagement/bdd_notifier.rb
@@ -19,26 +19,39 @@ class BDDNotifier
     @slack_id_dictionary[@slack_user_dictionary[git_name]]
   end
 
-  def say(word, git_user)
+  def slack_error(word, git_user)
     slack_id = find_slack_id_from_git_name(git_user)
     if slack_id.nil? || slack_id.size < 1
       raise "ERROR: Cannot find a slack user id that match git user: #{git_user}!"
     end
-    @client.chat_postMessage(channel: slack_id, text: word, as_user: true)
+    attachments = [
+            {
+                :color=>"danger",
+                :title=>"BDD Test Failed",
+                :text=>word
+            }
+        ]
+
+    @client.chat_postMessage(channel: slack_id, as_user: true, attachments: attachments)
   end
 
   def notify_bdd_failure(git_user, repo, build, cuc_tag, job_id)
-    message = "Build ##{build} of #{repo} failed. Please check: "
+    if git_user.present?
+      user = git_user
+    else
+      cmd = "git --no-pager show -s --format='%an <%ae>'"
+      user = `#{cmd}`.split(' ')[0]
+    end
+    message = "Build ##{build} of #{repo} failed. \nPlease check: "
     travis_link = "https://travis-ci.org/CBIIT/nci-uMatch-bddtests/jobs/#{job_id}"
     travis_link = "<#{travis_link}|Travis Log>"
-    message += "\n#{travis_link} or "
     today_date = Date.today.strftime('%m-%d-%y')
     bdd_report_link = 'http://pedmatch-admin-alb-external-382939701.us-east-1.elb.amazonaws.com:3025'
     bdd_report_link = "#{bdd_report_link}/report/#{today_date}/critical/#{cuc_tag.gsub('@', '')}"
     bdd_report_link = "<#{bdd_report_link}|BDD Test Report>"
-    message += "#{bdd_report_link}"
-    say(message, git_user)
+    message += "\n#{bdd_report_link} or #{travis_link}"
+    slack_error(message, user)
   end
 end
 
-# BDDNotifier.new.notify_bdd_failure('LiWang', 'nci-match-patient-api', '01-09-17-1048', '@ui', "190292979")
+BDDNotifier.new.notify_bdd_failure('', 'nci-match-patient-api', '01-09-17-1048', '@ui', "190292979")
