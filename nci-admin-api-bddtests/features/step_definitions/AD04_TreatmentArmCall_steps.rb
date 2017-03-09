9bd1f4655b651f35b20ccd26f9e642648a29a510
mraseelak <raseel.mohamed@nih.gov>
2017-03-09 17:12:00 +0530

    Adding test case for treatment_arm_api under admin tool api

---
+++ nci-admin-api-bddtests/features/step_definitions/AD04_TreatmentArmCall_steps.rb
@@ -0,0 +1,70 @@
+require_relative './spec_helper'
+
+Given(/^I build a request param with id "([^"]*)", stratum id "([^"]*)" and version "([^"]*)"$/) do |treatment_arm_id, stratum, version|
+	@treatment_arm_id = Utility.nil_if_empty(treatment_arm_id)
+	@stratum_id = 	Utility.nil_if_empty(stratum)
+	@version = Utility.nil_if_empty(version)
+
+	param_array = []
+	param_array << "treatment_arm_id=#{@treatment_arm_id}" unless @treatment_arm_id.nil?
+	param_array << "stratum_id=#{@stratum_id}" unless @stratum_id.nil?
+	param_array << "version=#{@version}" unless @version.nil?
+
+  @query = param_array.join('&')
+end
+
+Given(/^I build a request param with id "([^"]*)"$/) do |treatment_arm_id|
+  step %{I build a request param with id "#{treatment_arm_id}", stratum id "" and version ""}
+end
+
+Given(/^I build a request param with "([^"]*)" "([^"]*)" and "([^"]*)" "([^"]*)"$/) do |arg1, arg1_value, arg2, arg2_value|
+  default = {
+  	treatment_arm_id: '',
+  	stratum_id: '',
+  	version: ''
+  }
+
+  default[arg1.to_sym] = arg1_value
+  default[arg2.to_sym] = arg2_value
+
+  step %{I build a request param with id "#{default[:treatment_arm_id]}", stratum id "#{default[:stratum_id]}" and version "#{default[:version]}"}
+end
+
+When(/^I make a get call to treatment_arm_api with those parameters$/) do
+  request = "#{@admin_endpoint}/api/v1/admintool/treatment_arm_api?#{@query}"
+  
+  @response = Request.get_request(request)
+  puts @response
+end
+
+Then(/^I should retrieve one treatment arm$/) do
+	response_message = JSON.parse(@response[:message])
+	expect(response_message).to be_a Hash
+  expect(response_message.size).to eql(1);
+end
+
+Then(/^I should retrieve an array of treatment arm\(s\)$/) do
+	response_message = JSON.parse(@response[:message])
+  expect(response_message).to be_a Array
+end
+
+Then(/^All the "([^"]*)" in all the treatment arm is "([^"]*)"$/) do |field, value|
+	response_message = JSON.parse(@response[:message])
+  field_collection = response_message.map { |e| e[field.to_sym] }
+
+  field_collection.each do |f|
+  	expect(f).to eql(value)
+  end
+end
+
+Then(/^I should receive an empty hash$/) do
+  response_message = JSON.parse(@response[:message])
+  expect(response_message).to be_a Hash
+  expect(response_message.size).to eql(0);
+end
+
+Then(/^I should receive an empty array$/) do
+  response_message = JSON.parse(@response[:message])
+  expect(response_message).to be_a Array
+  expect(response_message.size).to eql(0)
+end
--- nci-admin-api-bddtests/support/request.rb
+++ nci-admin-api-bddtests/support/request.rb
@@ -18,7 +18,7 @@ module Request
   # status <String> Passed | Failed
   # message <String> Unaltered response body
   # 
-	def post_request(url, body={}, auth0_on=true, auth0_role='ADMIN')
+	def post_request(url, body={}, auth0_role='ADMIN', auth0_on=true)
 		puts "POST_URL: #{url}"
 		headers = {
 			content_type: :json, 
@@ -36,6 +36,14 @@ module Request
 				payload: body.to_json, 
 				headers: headers)
 
+      http_code = "#{response.code}"
+      status = http_code.match(/20(\d)/) ? 'Success' : 'Failure'
+      post_response['status'] = status
+      post_response['http_code'] = http_code
+      post_response['message'] = response.body
+      if status.eql?('Failure')
+        p post_response['message']
+      end
 		rescue StandardError => e
       post_response['status'] = 'Failure'
 
@@ -49,15 +57,58 @@ module Request
       end
       return post_response
     end
-
-    http_code = "#{response.code}"
-    status = http_code.match(/20(\d)/) ? 'Success' : 'Failure'
-    post_response['status'] = status
-    post_response['http_code'] = http_code
-    post_response['message'] = response.body
-    if status.eql?('Failure')
-      p post_response['message']
-    end
     post_response
 	end
+
+
+  ## Get Request
+  # @Params
+  # <String> url: Required. Complete url including query parameters.
+  # <Boolean> auth0_on: Optional. If True then a request is made to the auth0 endpoint to get a token
+  # <String> auth0_role: Optional. provide the role
+  # 
+  # @return
+  # This returns a hash object with the following. 
+  # status_code <String> the status code of the response message. 
+  # status <String> Passed | Failed
+  # message <String> Unaltered response body
+  # 
+  def get_request(url, auth0_role='ADMIN', auth0_on = true)
+    puts "GET url: #{url}"
+    headers = {
+      content_type: :json,
+      accept: :json
+    }
+
+    Auth0Client.add_auth0_header(headers, auth0_role) if auth0_on
+
+    get_response = {}
+
+    begin
+      response = RestClient::Request.execute(
+        url: url,
+        method: :get,
+        headers: headers
+        )
+
+      get_response['status']    = response.code.match(/20(\d)/) ? 'Success' : 'Failure'
+      get_response['http_code'] = response.code
+      get_response['message']   = response.message
+
+      if get_response['status'] == "Failure"
+        puts get_response['message']
+      end
+    rescue StandardError => err
+      get_response['status'] = 'Failure'
+      get_response['http_code'] = '500'
+
+      if err.respond_to? :response
+        get_response['message'] = err.response
+      else
+        get_response['message'] = err.message
+      end
+      return get_response
+    end
+    get_response
+  end
 end
