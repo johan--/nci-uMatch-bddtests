59c876d7956e1c30d7d1113ec23285ba647c26f0
mraseelak <raseel.mohamed@nih.gov>
2017-03-14 18:20:48 +0530

    Changing tier to server

--- nci-admin-api-bddtests/features/step_definitions/before.rb
+++ nci-admin-api-bddtests/features/step_definitions/before.rb
@@ -1,6 +1,6 @@
 require_relative './spec_helper'
 
 Before do
-	Environment.set_tier 'local' #set tier to local for developing tests. 
+	Environment.set_tier 'server' #set tier to local for developing tests. 
 	puts "Testing on tier #{Environment.get_tier}"
 end
