360c5b6aa4b9224a3e9b017ae762a38b111b5555
LiWang <adamastwl@gmail.com>
2017-10-16 10:42:54 -0400

    in bdd_reporter, add function to cleanup cucumber json report: remove duplicated background scenarios turned off UAT push

--- TestManagement/bdd_reporter.rb
+++ TestManagement/bdd_reporter.rb
@@ -12,44 +12,56 @@ class BddUploader
     upload_report(json_folder, @output_html_name, date, s3_bucket)
   end
 
-  private_class_method def self.generate_html(json_folder, level, tag, date)
-                         clean_json_files(json_folder)
-                         template_file = "#{File.dirname(__FILE__)}/generate_report_js_template.txt"
-                         js_file = "#{File.dirname(__FILE__)}/bdd_reporter/support/generate_report.js"
-                         title = "#{tag.sub('@', '').upcase} BDD Reports (#{level}, #{date})"
-                         template = File.read(template_file)
-                         template.gsub!('**json_folder**', json_folder)
-                         template.gsub!('**html_name**', @output_html_name)
-                         template.gsub!('**report_name**', title)
-                         file_write = File.open(js_file, 'w')
-                         file_write.puts(template)
-                         file_write.close
-                         cmd = "cd #{File.dirname(__FILE__)}/bdd_reporter; node support/generate_report.js"
-                         `#{cmd}`
-                         puts "HTML report file #{@output_html_name} is generated in #{json_folder}"
-                       end
+  def self.generate_html(json_folder, level, tag, date)
+    clean_json_files(json_folder)
+    template_file = "#{File.dirname(__FILE__)}/generate_report_js_template.txt"
+    js_file = "#{File.dirname(__FILE__)}/bdd_reporter/support/generate_report.js"
+    title = "#{tag.sub('@', '').upcase} BDD Reports (#{level}, #{date})"
+    template = File.read(template_file)
+    template.gsub!('**json_folder**', json_folder)
+    template.gsub!('**html_name**', @output_html_name)
+    template.gsub!('**report_name**', title)
+    file_write = File.open(js_file, 'w')
+    file_write.puts(template)
+    file_write.close
+    cmd = "cd #{File.dirname(__FILE__)}/bdd_reporter; node support/generate_report.js"
+    `#{cmd}`
+    puts "HTML report file #{@output_html_name} is generated in #{json_folder}"
+  end
 
-   def self.clean_json_files(json_folder)
-                         Dir["#{json_folder}/*"].each do |j|
-                           if j.end_with?('.json')
-                             begin
-                               JSON.parse(File.read(j))
-                             rescue Exception => e
-                               puts "#{j} is not a valid json, delete it"
-                               File.delete(j)
-                             end
-                           end
-                         end
-                       end
+  def self.clean_json_files(json_folder)
+    Dir["#{json_folder}/*"].each do |j|
+      if j.end_with?('.json')
+        begin
+          report_json = JSON.parse(File.read(j))
+        rescue Exception => e
+          puts "#{j} is not a valid json, delete it"
+          File.delete(j)
+        end
+        report_json.each do |r|
+          background_found = false
+          r['elements'].each do |e|
+            if e['keyword'] == 'Background'
+              r['elements'].delete(e) if background_found
+              background_found = true
+            end
+          end
+        end
+        file_write = File.open(j, 'w')
+        file_write.puts(report_json.to_json)
+        file_write.close
+      end
+    end
+  end
 
-  private_class_method def self.convert_html(report_path, date)
-                         puts 'Starting the conversion'
-                         html = load_html(report_path)
-                         convert_images(html, date)
-                         convert_links(html, date)
-                         build_file(html, date)
-                         puts 'Done'
-                       end
+  def self.convert_html(report_path, date)
+    puts 'Starting the conversion'
+    html = load_html(report_path)
+    convert_images(html, date)
+    convert_links(html, date)
+    build_file(html, date)
+    puts 'Done'
+  end
 
   private_class_method def self.load_html(html_path)
                          raise "File Name #{html_path} does not exist" unless File.exists? html_path
@@ -82,17 +94,16 @@ class BddUploader
                          file_write.close
                        end
 
-  private_class_method def self.upload_report(report_folder, report_file_name, date, s3_bucket)
-                         file_path = "#{report_folder}/#{report_file_name}"
-                         cmd = "aws s3 cp #{file_path} s3://#{s3_bucket}/#{date}/#{report_file_name} --region us-east-1"
-                         `#{cmd}`
-                         puts "Cucumber report #{file_path} has been uploaded to s3://#{s3_bucket}/#{date}"
-                         screenshot = "#{report_folder}/screenshot"
-                         if File.exist?(screenshot)
-                           cmd = "aws s3 cp #{screenshot} s3://#{s3_bucket}/#{date}/screenshot --region us-east-1 --recursive"
-                           `#{cmd}`
-                           puts "Screenshot #{screenshot} has been uploaded to s3://#{s3_bucket}/#{date}/screenshot"
-                         end
-                       end
+  def self.upload_report(report_folder, report_file_name, date, s3_bucket)
+    file_path = "#{report_folder}/#{report_file_name}"
+    cmd = "aws s3 cp #{file_path} s3://#{s3_bucket}/#{date}/#{report_file_name} --region us-east-1"
+    `#{cmd}`
+    puts "Cucumber report #{file_path} has been uploaded to s3://#{s3_bucket}/#{date}"
+    screenshot = "#{report_folder}/screenshot"
+    if File.exist?(screenshot)
+      cmd = "aws s3 cp #{screenshot} s3://#{s3_bucket}/#{date}/screenshot --region us-east-1 --recursive"
+      `#{cmd}`
+      puts "Screenshot #{screenshot} has been uploaded to s3://#{s3_bucket}/#{date}/screenshot"
+    end
+  end
 end
-BddUploader.clean_json_files('/Users/wangl17/match_apps/nci-uMatch-bddtests/results')
