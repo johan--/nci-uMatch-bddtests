0f94a7fd249a91f4ee8656e82ea49c97e267b8ee
Raseel Mohamed <raseel.mohamed@nih.gov>
2017-10-30 08:20:30 -0400

    Correcting the URL used for testing

--- nci-admin-api-bddtests/support/env.rb
+++ nci-admin-api-bddtests/support/env.rb
@@ -1,5 +1,5 @@
 ENV['HOSTNAME'] = 'https://admintool.umatch-int.nci.nih.gov:10260/api/v1/admintool/version'
-ENV['ADMIN_API_ENDPOINT'] = "https://admintool.umatch-int.nci.nih.gov:10260/api/v1/admintool/version"
+ENV['ADMIN_API_ENDPOINT'] = "https://admintool.umatch-int.nci.nih.gov:10260/api/v1/admintool"
 
 ENV['s3_bucket'] = 'test-admin-tool'
 ENV['s3_endpoint'] = 'https://s3.amazonaws.com'
